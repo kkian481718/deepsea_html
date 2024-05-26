c4e032232ffab4ff3a085a08439da2962458ddac
Miffy <39194102+kkian481718@users.noreply.github.com>
2024-05-26 20:58:29 +0800

    暫時

--- script.js
+++ script.js
@@ -10,74 +10,96 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
-const db = firebase.firestore()
-
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 // initialize data
 var score = new Array(8);
-
-var i = score.length;
-console.log("user number: ", i);
-
+var i = 7;
 for (k = 0; k < i; k++) {
-download_data(k);
+    download_data(k);
 }
 
 function download_data(userId) {
+    var docRef = db.doc(`users/${userId}`);
 
-var docRef = db.doc(`users/${userId}`);
+    docRef.onSnapshot((doc) => {
+        score[userId] = doc.data().userId;
+        document.getElementById('score_' + userId).textContent = score[userId];
+    });
+}
 
-docRef.onSnapshot((doc) => {
-    console.log(userId, "th user data:", doc.data().userId);
-    score[userId] = doc.data().userId;
-    document.getElementById('score_' + userId).textContent = score[userId];
-});
+// search
+function search_vaildate() {
+    var e1 = document.querySelector("#sea");
+    var e2 = document.querySelector("#year");
+    var e3 = document.querySelector("#month");
+    let sea_num = e1.options[e1.selectedIndex].value;
+    let year = e2.options[e2.selectedIndex].text;
+    let month = e3.options[e3.selectedIndex].value;
+    console.log(`sea: ${sea_num} /  year: ${year} / month: ${month}`);
 
+    if (sea_num === "") {
+        alert("請選擇海域");
+    }
+    else if (year === "請選擇年份") {
+        alert("請選擇年份");
+    }
+    else if (month === "") {
+        alert("請選擇月份");
+    }
+    else {
+        search(sea_num, year, month);
+    }
+}
+
+function search(sea_num, y, m) {
+    var newdocRef = db.doc(`deepseadata/sea${sea_num}/${y}${m}/data`);
+    console.log(`deepseadata/sea${sea_num}/${y}${m}/data`);
+
+    newdocRef.onSnapshot((doc) => {
+        var s = doc.data();
+        document.getElementById('test').textContent = s;
+        console.log(s);
+    });
 }
 
 // add
 function add(userId) {
-score[userId]++;
-document.getElementById('score_' + userId).textContent = score[userId];
+    score[userId]++;
+    document.getElementById('score_' + userId).textContent = score[userId];
 
-update_db(userId);
+    update_db(userId);
 }
 
 // minus
 function minus(userId) {
-score[userId]--;
-document.getElementById('score_' + userId).textContent = score[userId];
+    score[userId]--;
+    document.getElementById('score_' + userId).textContent = score[userId];
 
-update_db(userId);
+    update_db(userId);
 }
 
 // upload
 function update_db(userId) {
 
-const deviceDoc = db.doc(`users/${userId}`);
+    const deviceDoc = db.doc(`users/${userId}`);
 
-deviceDoc
-    .set({
-    userId: score[userId]
-    })
-    .then(() => console.log('Document successfully written!'))
-    .catch(error => console.error('Error writing document: ', error))
-
+    deviceDoc
+        .set({
+        userId: score[userId]
+        })
+        .then(() => console.log('Document successfully written!'))
+        .catch(error => console.error('Error writing document: ', error))
 }
 
+//DEBUG
 /*
-function update_log(userId) {
-
-const deviceDoc = db.doc(`users/log`);
-// get user ip
-
-deviceDoc
-    .set({
-    string:
-    })
-    .then(() => console.log('Document successfully written!'))
-    .catch(error => console.error('Error writing document: ', error))
-
+function search() {
+    let sea_num = $("#sea").val();
+    let year = $("#year option:selected").text();
+    let month = $("#month option:selected").text();
+    score[1] = year;
+    alert(`value${sea_num} text${year}`);
 }
 */
